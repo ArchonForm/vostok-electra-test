c837d9cb409ebbbd3ce08ce5b5c4bcb574761af3
ArchonForm <a.v.pogulyaev@gmail.com>
2020-06-10 01:15:41 +0500

    upd

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,38 +1,37 @@
 @extends('layouts.app')
-  
+
 @section('content')
 <div class="col-lg-6 offset-lg-3">
 
+    <div class="pull-left">
+        <h2>Add new User</h2>
+    </div>
+    <div class="pull-right">
+        <a class="btn btn-sm btn-primary" href="{{ route('users.index') }}"> Back</a>
+    </div>
 
-        <div class="pull-left">
-            <h2>Add new User</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-sm btn-primary" href="{{ route('users.index') }}"> Back</a>
-        </div>
-
-
-   
-@if ($errors->any())
+    @if ($errors->any())
     <div class="alert alert-danger">
         There were some problems with your input.<br><br>
         <ul>
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+            <li>{{ $error }}</li>
             @endforeach
         </ul>
     </div>
-@endif
-   <hr>
-<form action="{{ route('users.store') }}" method="POST">
-    @csrf
-  
-    <div class="col justify-content-center">
-        <div class="form-group">
-            <strong>Last Name:</strong>
-            <input type="text" name="last_name" class="form-control" required>
+    @endif
+
+    <hr>
+
+    <form action="{{ route('users.store') }}" method="POST">
+        @csrf
+
+        <div class="col justify-content-center">
+            <div class="form-group">
+                <strong>Last Name:</strong>
+                <input type="text" name="last_name" class="form-control" required>
+            </div>
         </div>
-    </div>
 
         <div class="col justify-content-center">
             <div class="form-group">
@@ -56,10 +55,10 @@
         </div>
 
         <div class="col-lg-2">
-                <button type="submit" class="btn btn-success">Submit</button>
+            <button type="submit" class="btn btn-success">Submit</button>
         </div>
-    </div>
-   
+</div>
+
 </form>
 
 </div>
